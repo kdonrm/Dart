f39355e90431104d3c4b275ffdce1a4beb1acd4d
Kalapuge Don Ravindu Madusnka <kdonrm2000@gmail.com>
2024-09-17 12:06:52 +0530

    Dart example.

--- main.dart
+++ main.dart
@@ -1,3 +1,16 @@
-void main() { 
-  print("Hello World!"); 
+import 'dart:ffi';
+
+void main(){
+  int x = 2 + 3;
+  print(x);
+
+  int y = 100 - 50;
+  print(y);
+
+  int z = 2 * 10;
+  print(z);
+
+  double m = 100/5;
+  print(m);
+
 }
